f03815d3b80f0ef116da36fdbb2b606c3cf0182c
Pramesh <pramesh_morang@hotmail.com>
2016-10-17 17:55:56 +0300

    change in code

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-// bring in the necessary dependencies
+//bring in the necessary dependencies
 var express = require('express');
+var cors = require ('cors');
 var bodyParser = require('body-parser');
-var cors = require('cors');
+
 
 // Configure the express web server framework
 var app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// Define the API routes for our application
+
+//define the API routes for our application
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 var messages = [
-    {
-        username: "Mr. A",
-        message: "Hello world"
-    },
-    {
-        username: "Mr. B",
-        message: "Good day"
-    }
-
+  {
+  username: "Richard",
+  message: "Hello"
+  },
+  {
+    username: "Rick",
+    message: "Hello sire"
+  }
 ];
+//messages
+//get
+//posts
 
-//
-// /messages
-// get
-// post
 app.get('/messages', function (req, res) {
-    res.json(messages);
+  res.json(messages);
 });
 
-app.post('/messages', function (req, res) {
-    messages.push({
-        username: req.body.username,
-        message: req.body.message
-    });
-    res.json(messages);
+app.post('/messages',  function (req, res) {
+  messages.push({
+    username:  req.body.username,
+    message:  req.body.message
+  });
+  res.json(messages);
 });
 
 
-// Activate the express server
+//Activate the express server
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening');
+  console.log('Example app listening on port');
 });
